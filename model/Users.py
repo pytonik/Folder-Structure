79e3651e7116bf4aac08af96376ffd949933d2f3
pytonik <57829979+pytonik@users.noreply.github.com>
2019-11-17 03:44:44 +0100

    pytonik Folder-Structure

--- model/Users.py
+++ model/Users.py
@@ -1,16 +1,18 @@
 from pytonik import Web
 
-def insert_users(data):
-    db = Web.Model().db
-    query_register = "INSERT INTO usertable (firstnamamvc, lastnamemvc, usernamemvc, passwordmvc, createddatemvc) " \
-                     "VALUES " \
-                     "(%(firstnamamvc)s, %(lastnamemvc)s, %(usernamemvc)s, %(passwordmvc)s, %(createddatemvc)s)"
-    db.query(query_register, data)
-    return db.save()
+from pytonik import Web
 
 
-def check_user_count(useremail):
-    db = Web.Model().db
-    query_check = "SELECT * FROM usertable WHERE usernamemvc = '{usernamemvc}'".format(usernamemvc=useremail)
-    db.query(query_check)
-    return db.countall()
+class Users:
+
+    def __getattr__(self, item):
+        return item
+
+    def __init__(self):
+        self.db = Web.Model().db
+
+    def list(self):
+
+        # database query here
+
+        return ""
