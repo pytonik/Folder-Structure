8123b1ece9b676f954fbc98d52a71af7de3ae3d0
Raphael Essien <emmaessiensp@gmail.com>
2020-05-16 09:27:35 -0500

    Update routes.py

--- routes.py
+++ routes.py
@@ -4,26 +4,4 @@ route = Router()
 
 route.get("/")
 
-route.any('fff', 'profileController@index:id:user')
 
-route.any('login', 'AccountController@signin')
-
-route.any('register', 'AccountController@signup')
-
-route.any('recover', 'AccountController@recover')
-
-route.get('logout', 'AccountController@signout')
-
-route.get('first', 'AccountController@update_user_step_one')
-
-route.any('professionals', 'indexController@professionals')
-
-route.any('companies', 'indexController@companies')
-
-#route.any('info', 'profileController@info')
-
-
-route.get('profile/follow', route.args(
-    params=['id']
-
-))
